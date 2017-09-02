b0e358bc3b7da741c5e51889ae02bf7b276f6b08
CallumI <callum.iddon@gmail.com>
2017-09-02 22:16:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KaTeX Quick Render - Atom Package
+
+An atom plugin to render LaTeX maths equations instantly using KaTeX. Avoid waiting while your whole pdf compiles.
---
+++ spec/katex-quick-render-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KatexQuickRenderView from '../lib/katex-quick-render-view';
+
+describe('KatexQuickRenderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
